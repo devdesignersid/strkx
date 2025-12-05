0adba7838525196b2eabea1af32299321e23398f
Siddharth S Kumar <dev.designer.sid@gmail.com>
2025-12-05 23:23:53 +0530

    feat: Integrate DashboardModule into UserModule and delegate dashboard cache invalidation to DashboardService.

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { DashboardModule } from '../dashboard/dashboard.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, DashboardModule],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
